1008a9c1568e97cdafa1c5185cd15e1b863b4dea
htmluiv <73861559+htmluiv@users.noreply.github.com>
2020-11-03 11:15:04 +0900

    11
    111

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(640, 400);
+}
+
+function draw() {
+  background(51);
+  stroke(255);
+  noFill();
+  drawCircle(width/2, height/2, 300);
+}
+
+function drawCircle(x, y, d)  {
+  ellipse(x, y, d, d);
+  
+  if(d>10)  {
+  drawCircle(x+d/2, y, d/2);
+  drawCircle(x-d/2, y, d/2);
+  }
+}
+
